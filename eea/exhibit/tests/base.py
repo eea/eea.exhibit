723d696b6d459fa75eefd6d40b5e4476fb7763d1
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-01-19 13:05:10 +0000

    - Hudson fix: Added tests framework.

---
+++ eea/exhibit/tests/base.py
@@ -0,0 +1,34 @@
+""" Tests setup
+"""
+from Products.PloneTestCase import PloneTestCase
+from Products.PloneTestCase.layer import onsetup
+from Products.Five import zcml
+from Products.Five import fiveconfigure
+
+PRODUCTS = ['FiveSite', 'eea.exhibit']
+
+@onsetup
+def setup_eea_exhibit():
+    """ Setup
+    """
+    fiveconfigure.debug_mode = True
+    import Products.Five
+    import Products.FiveSite
+    import eea.exhibit
+    zcml.load_config('meta.zcml', Products.Five)
+    zcml.load_config('configure.zcml', Products.Five)
+    zcml.load_config('configure.zcml', Products.FiveSite)
+    zcml.load_config('configure.zcml', eea.exhibit)
+    fiveconfigure.debug_mode = False
+
+    PloneTestCase.installProduct('Five')
+    for i in PRODUCTS:
+        PloneTestCase.installProduct(i)
+
+setup_eea_exhibit()
+PloneTestCase.setupPloneSite(products=PRODUCTS)
+
+
+class EEAExhibitFunctionalTestCase(PloneTestCase.FunctionalTestCase):
+    """ Functional test case
+    """
---
+++ eea/exhibit/tests/test_doctests.py
@@ -0,0 +1,25 @@
+import doctest
+import unittest
+from eea.exhibit.tests.base import EEAExhibitFunctionalTestCase
+from Testing.ZopeTestCase import FunctionalDocFileSuite
+
+
+OPTIONFLAGS = (
+        doctest.REPORT_ONLY_FIRST_FAILURE |
+        doctest.ELLIPSIS |
+        doctest.NORMALIZE_WHITESPACE
+)
+
+def test_suite():
+    """ Tests
+    """
+    return unittest.TestSuite((
+            FunctionalDocFileSuite('README.txt',
+                  optionflags=OPTIONFLAGS,
+                  package='eea.exhibit',
+                  test_class=EEAExhibitFunctionalTestCase),
+              ))
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
