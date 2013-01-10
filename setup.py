a854be9e134b5c34d457b02ed1e1c39cff57e32a
Alin Voinea <alin@eaudeweb.ro>
2013-01-10 16:19:03 +0200

    Prepare eea.exhibit 6.2.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(name=NAME,
       install_requires=[
           'setuptools',
           'rdflib',
-          'eea.app.visualization >= 6.1',
+          'eea.app.visualization >= 6.2',
           # -*- Extra requirements: -*-
       ],
       extras_require={
