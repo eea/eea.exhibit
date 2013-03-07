dcd021fe2c958c9859f31e1d9b58e4047097b08b
Alin Voinea <alin@eaudeweb.ro>
2013-03-07 16:35:55 +0200

    Refs #14003 - Load exhibit in minified or not according with portal_js
    - Load exhibit-api JS minified if portal_javascript is not in
      development mode and viceversa in order to easily debug JS errors

--- eea/exhibit/browser/app/view.py
+++ eea/exhibit/browser/app/view.py
@@ -1,8 +1,10 @@
 """ Browser views
 """
+import json
 from zope.component import queryAdapter
 from Products.Five.browser import BrowserView
 from eea.app.visualization.interfaces import IVisualizationConfig
+from eea.app.visualization.zopera import getToolByName
 
 class View(BrowserView):
     """ Custom header
@@ -11,6 +13,7 @@ class View(BrowserView):
         super(View, self).__init__(context, request)
         self._accessor = None
         self._available = None
+        self._bundle = None
 
     @property
     def accessor(self):
@@ -27,11 +30,26 @@ class View(BrowserView):
         if self._available is None:
             for view in self.accessor.views:
                 name = view.get('name', '')
+                if name == u'daviz.properties':
+                    continue
                 if name.startswith('daviz.') or name.startswith('exhibit.'):
                     self._available = True
                     break
         return self._available
 
+    @property
+    def bundle(self):
+        """ Enable bundle
+        """
+        if self._bundle is not None:
+            return self._bundle
+
+        self.jstool = getToolByName(self.context, 'portal_javascripts', None)
+        if self.jstool:
+            self._bundle = not self.jstool.getDebugMode()
+        else:
+            self._bundle = True
+        return json.dumps(self._bundle)
 
     @property
     def sources(self):
