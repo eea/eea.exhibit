4382b99426cf5a0086b8fa3059858664cefe53f0
Alin Voinea <alin@eaudeweb.ro>
2013-03-12 16:56:55 +0200

    [JENKINS] - Fix wrong dependency

--- eea/exhibit/controlpanel/controlpanel.py
+++ eea/exhibit/controlpanel/controlpanel.py
@@ -1,11 +1,11 @@
-""" Googlecharts Control Panel Section
+""" Control Panel Section
 """
 from zope.interface import implements
 from eea.app.visualization.controlpanel.interfaces import IDavizSection
 from zope.formlib.form import FormFields
 from zope import schema
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-from eea.googlecharts.config import EEAMessageFactory as _
+from eea.app.visualization.config import EEAMessageFactory as _
 
 class ExhibitSection(object):
     """ Simile Exhibit Settings Section
